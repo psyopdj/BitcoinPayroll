558ae4920f597f5d248647132909c5d13e887cf8
Tyler Carlson <tjcarlson25@gmail.com>
2022-11-29 22:08:44 -0600

    added pay rate to employee

--- src/api/model/employee.py
+++ src/api/model/employee.py
@@ -2,11 +2,12 @@ import json
 
 
 class Employee:
-    def __init__(self, _id=None, rfid=None, name=None, wallet_address=None):
+    def __init__(self, _id=None, rfid=None, name=None, wallet_address=None, pay_rate=None):
         self._id = _id
         self.rfid = rfid
         self.name = name
         self.wallet_address = wallet_address
+        self.pay_rate = pay_rate
 
     def __iter__(self):
         yield from {
@@ -14,13 +15,15 @@ class Employee:
             "rfid": self.rfid,
             "name": self.name,
             "wallet_address": self.wallet_address,
+            "pay_rate": self.pay_rate
         }.items()
 
     def __str__(self):
         return json.dumps(self.to_json())
 
     def to_json(self):
-        json_dct = {"rfid": self.rfid, "name": self.name, "wallet_address": self.wallet_address}
+        json_dct = {"rfid": self.rfid, "name": self.name, "wallet_address": self.wallet_address,
+                    "pay_rate": self.pay_rate}
         # only add _id if it exists in the object already
         if self._id:
             json_dct['_id'] = str(self._id)
@@ -28,7 +31,8 @@ class Employee:
 
     @staticmethod
     def from_json_dict(json_dict):
-        return Employee(json_dict.get('_id', None), json_dict['rfid'], json_dict['name'], json_dict['wallet_address'])
+        return Employee(json_dict.get('_id', None), json_dict['rfid'], json_dict['name'], json_dict['wallet_address'],
+                        json_dict['pay_rate'])
 
     @staticmethod
     def from_json_str(json_str):
@@ -36,12 +40,13 @@ class Employee:
         return Employee.from_json_dict(json_dict)
 
     def __hash__(self):
-        return (self._id, self.rfid, self.name, self.wallet_address).__hash__()
+        return (self._id, self.rfid, self.name, self.wallet_address, self.pay_rate).__hash__()
 
     def __eq__(self, other):
         if isinstance(other, Employee):
             return self._id == other._id and \
                    self.rfid == other.rfid and \
                    self.name == other.name and \
-                   self.wallet_address == other.wallet_address
+                   self.wallet_address == other.wallet_address and \
+                   self.pay_rate == other.pay_rate
         return False
